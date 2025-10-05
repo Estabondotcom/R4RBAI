f88a301d83a042fd02c7a75a3f0c8752caa96b59
Estabondotcom <franksalinger23@gmail.com>
2025-10-04 21:16:33 -0400

    replace repo with local version

--- firebaseConfigShim.js
+++ firebaseConfigShim.js
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyCV8U5deVGvGBHn00DtnX6xkkNZJ2895Qo",
   authDomain: "r4rbai.firebaseapp.com",
   projectId: "r4rbai",
-  storageBucket: "r4rbai.firebasestorage.app",
+  storageBucket: "r4rbai.appspot.com",
   messagingSenderId: "289173907451",
   appId: "1:289173907451:web:86afa1cc2b0610bf56cd5e",
   measurementId: "G-G0MSMZ3C9X"
